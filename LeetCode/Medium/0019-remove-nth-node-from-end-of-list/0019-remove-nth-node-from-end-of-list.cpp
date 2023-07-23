9d0c3f8be640e7568238c8e4a94e5d20b50650b3
Coding_Pooh <tjdals4047@gmail.com>
2023-07-23 10:45:23 +0900

    Time: 0 ms (100.00%), Space: 10.6 MB (40.85%) - LeetHub

--- LeetCode/Medium/0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ LeetCode/Medium/0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -15,28 +15,26 @@ public:
         ListNode S;
         S.next = head;
 
-
-        ListNode* q = S.next;
-        // get length of linked list
+        // 1. Linked list 길이 구하기
+        ListNode* temp = head;
         int l = 0;
-        while(q != nullptr){
+        while(temp != nullptr){
             l++;
-            q = q->next;
+            temp = temp->next;
         }
 
+        // 2. 제거할 노드 찾기
         ListNode* p = &S;
 
-        for (int i = 0; i< l-n ; i++){
+        // 제거해야할 노드의 바로 이전 노드를 p로 저장
+        for (int i = 0; i < l-n ; i++){
             p = p -> next;
         }
 
-        ListNode* removedNode = p->next;
+        // 3. 노드 제거
+        p->next = p->next->next;
+        
+        return S.next;
 
-        if(removedNode == nullptr){
-            return nullptr;
-        }else{
-            p->next = removedNode->next;
-            return S.next;
-        }
     }
 };
