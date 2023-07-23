52979ac2da56862e8b5cbfbdcedec534de634601
Coding_Pooh <tjdals4047@gmail.com>
2023-07-23 11:14:12 +0900

    Time: 124 ms (61.90%), Space: 63.6 MB (22.09%) - LeetHub

--- LeetCode/Medium/0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ LeetCode/Medium/0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        
-        int* arr = new int[nums.size()];
+
+        // initializing array with variable size length filled with 0
+        int* arr = new int[nums.size()] ();
 
         for(int i = 0 ; i < nums.size() ; i++){
-            if (arr[nums[i]-1] == -1){
+            if (arr[nums[i]] == -1){
                 return nums[i];
             }else{
-                arr[nums[i]-1] = -1;
+                arr[nums[i]] = -1;
             }
         }
 
