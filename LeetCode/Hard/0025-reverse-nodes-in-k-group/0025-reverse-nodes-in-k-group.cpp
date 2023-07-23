8ccbb2ac9864832d4e65dab85446e3347a8b7ab3
Coding_Pooh <tjdals4047@gmail.com>
2023-07-23 18:31:07 +0900

    Time: 20 ms (46.75%), Space: 11.4 MB (41.44%) - LeetHub

--- LeetCode/Hard/0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ LeetCode/Hard/0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -13,78 +13,61 @@ public:
     ListNode* reverseKGroup(ListNode* head, int k) {
         if (k==1) return head;
         
-        ListNode* prevlast;
+        ListNode reversed(0, head);
+        ListNode* p = &reversed;
+
         ListNode* check;
         ListNode* first;
+        ListNode* last;
 
         ListNode* curr;
         ListNode* after1;
         ListNode* after2;
         
-        ListNode* p = head;
-        
-        int n = 0;
-        
+        ListNode* prevlast;
+
         while(p!=nullptr){
             
-            // 1. k개의 묶음이 만들어질 수 있는 지 확인
-            check = p;
+            prevlast = p;
 
-            for(int i = 1; i <= k; i++){
+            // 1. k개의 묶음이 만들어질 수 있는 지 확인 --> 만들어진다면 p가 last에서 멈춤
+            for(int i = 0; i < k; i++){
+                p = p->next;
 
                 // k개의 묶음이 안만들어지면 바로 return
-                if (check == nullptr){
-                    return head;  
-                }
+                if (p == nullptr) return reversed.next;
 
-                // 나중에 사용하려고 묶음의 첫번째 저장
-                if(i==1) first = check;
-                
-                // k번만큼 이동. 마지막에는 다음묶음의 첫번째 node 혹은 nullptr이 됨
-                check = check->next;
+                if(i==0) first = p;
             }
 
-            // 묶음 개수(뒤에서 1이면 head연결해주려고)
-            n++;
-
             // 2. 묶음이 존재하니 k개를 reverse
-            curr = p;
-            after1 = curr->next;
-            after2 = curr->next->next; 
+            curr = first;             // 묶음의 첫번째
+            after1 = curr->next;        // 묶음의 두번째
+            after2 = curr->next->next;  // 묶음의 세번째
 
-            for(int i = 1; i <= k-1; i++){
+            for(int i = 0; i < k-1; i++){
                 after1->next = curr;
 
-                if(i==k-1) break;
+                if(i==k-2) break;
 
+                // 한칸씩 뒤로 이동
                 curr = after1;
                 after1 = after2;
                 after2 = after1->next;
-
             }
 
-            // 현재 revered 묶음에서 다음 묶음 연결
-            first->next = check;
+            // 이전 묶음의 마지막을 reversed된 현재 묶음의 첫번째와 연결
+            prevlast->next = p;
 
-            // head 혹은 이전 묶음 마지막 node랑 연결
-            if (n == 1){
-                // 첫번째 묶음이면 head 연결
-                head = after1;
-            }else{
-                // 두번째 이상의 묶음이면 이전 revered 묶음 마지막 노드랑 
-                // 이번 reversed 묶음 첫번째 노드랑 연결
-                prevlast->next = after1;
-            }
+            // reversed 묶음과 다음 묶음을 연결
+            first->next = after2;
             
-            // 이전 k개 묶음에서 마지막 노드 기록. 다음 묶음의 첫번째랑 연결해주려고
-            prevlast = first;
-
-            // 이제 다음묶음의 시작점으로 이동
-            p = check;
+            // p를 현재 묶음의 마지막으로
+            p = first;
+            
         }
 
-        return head;
-
+        return reversed.next;
 
     }
 };
