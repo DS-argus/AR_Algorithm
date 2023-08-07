ea75fdb15e2e931d2cca42a72daf3aad41036f59
Coding_Pooh <tjdals4047@gmail.com>
2023-08-07 15:45:47 +0900

    Time: 1073 ms (28.68%), Space: 187.8 MB (27.25%) - LeetHub

--- LeetCode/Medium/0015-3sum/0015-3sum.cpp
+++ LeetCode/Medium/0015-3sum/0015-3sum.cpp
@@ -1,53 +1,28 @@
 class Solution {
 public:
-
     vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> output;
+        set<vector<int>> s;
 
+        int j, k;
+        int len = nums.size()-1;
         sort(nums.begin(), nums.end());
-        
-        vector<vector<int>> result;
 
-        for(int i = 0; i<nums.size()-1; i++){
-            
-            if(i != 0 && nums[i-1] == nums[i]) continue;
-
-            int l = i+1;
-            int r = nums.size()-1;
-
-            while(l<r){
-                
-                int L = nums[l];
-                int R = nums[r];
-                int threeSum = nums[i] + L + R;
-
-                if (threeSum < 0){
-                    while(l<r){
-                        l++;
-                        if(L != nums[l]) break;
-                    }
-                    
-                }else if (threeSum > 0){
-                    
-                    while(l<r){
-                        r--;
-                        if(R != nums[r]) break;
-                    }
-
-                }else{
-                    
-                    vector<int> subresult{nums[i], nums[l], nums[r]};
-                    result.push_back(subresult);
-                    while(l<r){
-                        l++;
-                        if(L != nums[l]) break;
-                    }
-
+        for(int i=0; i<nums.size()-2; i++){
+            j = i+1;
+            k = nums.size()-1;
+            while(j < k){
+                int sum = nums[j] + nums[k];
+                if(sum < -nums[i]) j++;
+                else if(sum > -nums[i]) k--;
+                else{
+                    s.insert({nums[i], nums[j], nums[k]});
+                    j++;
+                    k--;
                 }
-            
             }
-
         }
-
-        return result;
+        output.assign(s.begin(),s.end());
+        return output;
     }
 };
