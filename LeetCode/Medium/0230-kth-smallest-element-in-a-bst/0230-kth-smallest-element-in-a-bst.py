b46aee10ee801fa7fb5066419736bd025fe1bac1
Coding_Pooh <tjdals4047@gmail.com>
2023-08-14 00:59:43 +0900

    Time: 59 ms (71.83%), Space: 20.6 MB (15.31%) - LeetHub

--- LeetCode/Medium/0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ LeetCode/Medium/0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -24,7 +24,6 @@ class Solution:
                     q.append(cntNode.left)
                     q.append(cntNode.right)
         
-        print(result)
         result.sort()
 
         return result[k-1]
