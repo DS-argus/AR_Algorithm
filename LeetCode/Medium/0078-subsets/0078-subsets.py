9afe41dbbee901fb0a7d4cc4290376b46f50020d
Coding_Pooh <tjdals4047@gmail.com>
2023-08-16 23:31:25 +0900

    Time: 40 ms (82.07%), Space: 16.6 MB (11.14%) - LeetHub

--- LeetCode/Medium/0078-subsets/0078-subsets.py
+++ LeetCode/Medium/0078-subsets/0078-subsets.py
@@ -2,18 +2,17 @@ class Solution:
 
     def subsets(self, nums: List[int]) -> List[List[int]]:
         result = []
-
         q = deque()
         q.append([])
 
         while q:
             subSet = q.popleft()
+            
             result.append(subSet)
 
-            if subSet is not None:
-                for num in nums:
-                    if subSet == [] or subSet[-1] < num:
-                        nextSubset = subSet + [num]
-                        q.append(nextSubset)
+            for num in nums:
+                if subSet == [] or subSet[-1] < num:
+                    nextSubset = subSet + [num]
+                    q.append(nextSubset)
         
         return result
