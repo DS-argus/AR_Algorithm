3f509c352a8757e4fdc5c3fec8def3a4700db661
Coding_Pooh <tjdals4047@gmail.com>
2023-08-22 01:47:30 +0900

    Time: 5808 ms (66.16%), Space: 16.4 MB (36.47%) - LeetHub

--- LeetCode/Medium/0079-word-search/0079-word-search.py
+++ LeetCode/Medium/0079-word-search/0079-word-search.py
@@ -14,19 +14,20 @@ class Solution:
                      self.DFSHelper(board, i, j-1, word, word_idx+1, prev_matrix) + \
                      self.DFSHelper(board, i-1, j, word, word_idx+1, prev_matrix)
             
+            # 
             prev_matrix[i][j] = 1
 
             return result 
-            
+
         return 0
 
     def exist(self, board: List[List[str]], word: str) -> bool:
 
         r, c = len(board), len(board[0])
 
+        prev_matrix = [[1 for _ in range(c+1)] for _ in range(r+1)]
         for m in range(r):
             for n in range(c):
-                prev_matrix = [[1 for _ in range(c+1)] for _ in range(r+1)]
                 if self.DFSHelper(board, m, n, word, 0, prev_matrix):
                     return True
                 
