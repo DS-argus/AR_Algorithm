2ae6ccd48688fa9122cbe11b6487b02d4f69739b
Coding_Pooh <tjdals4047@gmail.com>
2023-08-22 23:37:48 +0900

    Time: 3 ms (57.14%), Space: 15.8 MB (35.65%) - LeetHub

---
+++ LeetCode/Medium/0078-subsets/0078-subsets.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> elements;
+        vector<vector<int>> result;
+        vector<int>::iterator it = nums.begin();
+        
+        subsets(nums, elements, result, it); 
+        
+        return result;
+    }
+    
+    void subsets(vector<int>& nums, vector<int> elements, vector<vector<int>>& result, vector<int>::iterator it) {
+        if (it == nums.end()) {
+            result.push_back(elements);
+            return;
+        }
+        elements.push_back(*it);
+        subsets(nums, elements, result, it+1);
+
+        elements.pop_back();
+        subsets(nums, elements, result, it+1);
+    }
+};
