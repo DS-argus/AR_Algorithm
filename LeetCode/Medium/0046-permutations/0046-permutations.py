1430b632336877130c5a15cafb3b461b8b643d02
Coding_Pooh <tjdals4047@gmail.com>
2023-08-23 12:08:28 +0900

    Time: 47 ms (58.68%), Space: 16.6 MB (36.44%) - LeetHub

--- LeetCode/Medium/0046-permutations/0046-permutations.py
+++ LeetCode/Medium/0046-permutations/0046-permutations.py
@@ -1,20 +1,30 @@
-class Solution:
-
-    def permute(self, nums: List[int]) -> List[List[int]]:
-        result = []
-
-        if (len(nums) == 1):
-            return [nums[:]]
-
-        for i in range(len(nums)):
-            n = nums.pop(0)
-            
-            perms = self.permute(nums)
-
-            for perm in perms:
-                perm.append(n)
-            result.extend(perms)
-
-            nums.append(n)
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        self.lens = len(nums)
+        self.ids = [ele for ele in range(self.lens)]
+        self.output = []
+        self.nums = sorted(nums)
         
-        return result
+        self.permute_helper(self.ids, [])
+
+        return self.output
+
+    def permute_helper(self, id, temp) :
+        if id == [] :
+            self.output.append(temp)
+            return
+
+        else :
+            for i in id :
+                temps = temp.copy()
+                # temps = deepcopy(temp)
+                temps.append(self.nums[i])
+                
+                temp_id = id.copy()
+                # temp_id = deepcopy(id)
+                temp_id.remove(i)
+                self.permute_helper(temp_id, temps)
