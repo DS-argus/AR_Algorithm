d88b2f39403fd574c4f1641de4ea2cea293e840d
Coding_Pooh <tjdals4047@gmail.com>
2023-08-28 17:03:51 +0900

    Time: 31 ms (98.16%), Space: 16.7 MB (9.93%) - LeetHub

--- LeetCode/Medium/0078-subsets/0078-subsets.py
+++ LeetCode/Medium/0078-subsets/0078-subsets.py
@@ -1,17 +1,49 @@
 class Solution:
-    def __subsetsHelp(self, nums: list[int], result: list[list[int]]) -> None:
-        if len(nums)==0:
-            return
-        
-        if nums not in result:
-            result.append(nums)
-                    
-        for num in nums:
-            nextNums = nums.copy()
-            nextNums.remove(num)
-            self.__subsetsHelp(nextNums, result)
+    def subsets(self, nums: List[int]) -> List[List[int]]:
 
-    def subsets(self, nums: list[int]) -> list[list[int]]:
-        result = [[]]
-        self.__subsetsHelp(nums, result)
-        return result
+        nums.sort()
+        res = []
+        q = deque()
+        q.append([])
+
+        while q:
+            subset = q.popleft()
+
+            if subset not in res:
+                res.append(subset.copy())
+
+            for num in nums:
+                if subset == [] or num > subset[-1]:
+                    temp = subset.copy()
+                    temp.append(num)
+                    q.append(temp)
+        
+        return res
+
+            
+            
+            
+
+
+
+
+
+
+
+        # res = []
+
+        # subset = []
+
+        # def dfs(i):
+        #     if i >= len(nums):
+        #         res.append(subset.copy())
+        #         return
+        #     # decision to include nums[i]
+        #     subset.append(nums[i])
+        #     dfs(i + 1)
+        #     # decision NOT to include nums[i]
+        #     subset.pop()
+        #     dfs(i + 1)
+
+        # dfs(0)
+        # return res
