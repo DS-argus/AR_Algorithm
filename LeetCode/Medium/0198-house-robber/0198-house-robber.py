73f9df9b6131878c8b2afe8d7292644d02b5f90f
Coding_Pooh <tjdals4047@gmail.com>
2023-09-02 20:26:44 +0900

    Time: 41 ms (62.09%), Space: 16.2 MB (70.64%) - LeetHub

--- LeetCode/Medium/0198-house-robber/0198-house-robber.py
+++ LeetCode/Medium/0198-house-robber/0198-house-robber.py
@@ -1,19 +1,27 @@
 class Solution:
 
     def rob(self, nums: List[int]) -> int:
+        # DP list 정의
+        # DP[i] = nums[:i+1]에서 얻을 수 있는 maximum amount
         DP = [0] * len(nums)
 
-        for i in range(1, len(nums)+1):
-            subList = nums[:i]
+        # DP list 채우기
+        for i in range(len(nums)):
             
-            if len(subList) == 1:
-                DP[i-1] = nums[i-1]
+            # i == 0 , i == 1인 경우는 따로. 점화식에 DP[i-2] 들어가서
+            # DP[0] = nums[0]
+            if i == 0:
+                DP[i] = nums[i]
                 continue
-            elif len(subList) == 2:
-                DP[i-1] = max(subList[0], subList[1])
+
+            # DP[1] = nums[0], nums[1] 중 더 큰 값
+            if i == 1:
+                DP[i] = max(nums[0], nums[1])
                 continue
             
-            DP[i-1] = max(nums[i-1] + DP[i-3], DP[i-2])
+            # 1. nums[i-1]을 선택한 경우 : DP[i-1]과 동일
+            # 2. nums[i-1]을 선택하지 않은 경우 : DP[i-2] + nums[i] 
+            DP[i] = max(nums[i] + DP[i-2], DP[i-1])
 
-
+        # DP list 마지막 값 return
         return DP[-1]
