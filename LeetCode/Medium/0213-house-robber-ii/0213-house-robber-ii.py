78eea957b844f5ce5b6c332b2c2a325374b6b5fc
Coding_Pooh <tjdals4047@gmail.com>
2023-09-02 20:25:44 +0900

    Time: 34 ms (92.58%), Space: 16.4 MB (40.6%) - LeetHub

--- LeetCode/Medium/0213-house-robber-ii/0213-house-robber-ii.py
+++ LeetCode/Medium/0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,31 +1,50 @@
 class Solution:
 
+    # House Robber 1의 답
+    # nums의 양끝이 연결되어 있지 않음
     def noRotationRob(self, nums: List[int]) -> int:
+        # DP list 정의
+        # DP[i] = nums[:i+1]에서 얻을 수 있는 maximum amount
         DP = [0] * len(nums)
 
-        for i in range(1, len(nums)+1):
-            subList = nums[:i]
+        # DP list 채우기
+        for i in range(len(nums)):
             
-            if len(subList) == 1:
-                DP[i-1] = nums[i-1]
+            # i == 0 , i == 1인 경우는 따로. 점화식에 DP[i-2] 들어가서
+            # DP[0] = nums[0]
+            if i == 0:
+                DP[i] = nums[i]
                 continue
-            elif len(subList) == 2:
-                DP[i-1] = max(subList[0], subList[1])
+
+            # DP[1] = nums[0], nums[1] 중 더 큰 값
+            if i == 1:
+                DP[i] = max(nums[0], nums[1])
                 continue
             
-            DP[i-1] = max(nums[i-1] + DP[i-3], DP[i-2])
+            # 1. nums[i-1]을 선택한 경우 : DP[i-1]과 동일
+            # 2. nums[i-1]을 선택하지 않은 경우 : DP[i-2] + nums[i] 
+            DP[i] = max(nums[i] + DP[i-2], DP[i-1])
 
-
+        # DP list 마지막 값 return
         return DP[-1]
 
+
+    # House Robber 2
     def rob(self, nums: List[int]) -> int:
+        # nums 개수가 1, 2, 3인 경우는 따로, 개수 4 이상부터 nums[2:-1] 가능
         if len(nums) == 1:
             return nums[0]
+
         elif len(nums) == 2:
             return max(nums[0], nums[1])
+
         elif len(nums) == 3:
             return max(nums[0], nums[1], nums[2])
+
         else:
-            return max(self.noRotationRob(nums[1:]), self.noRotationRob(nums[2:-1]) + nums[0])
+            # 1. 첫번째를 선택하지 않는 경우 : nums[1:]에서 max 찾기
+            # 2. 첫번째를 선택하는 경우 : nums[2:-1]에서 max 찾기 + nums[0]
+            return max(self.noRotationRob(nums[1:]), \
+                       self.noRotationRob(nums[2:-1]) + nums[0])
 
     
