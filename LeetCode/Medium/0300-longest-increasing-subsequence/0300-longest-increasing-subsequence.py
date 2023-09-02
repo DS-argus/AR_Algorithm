8806f3bd9df8ae96b31d14863e8720e3d164680d
Coding_Pooh <tjdals4047@gmail.com>
2023-09-02 20:41:08 +0900

    Time: 2457 ms (48.77%), Space: 16.6 MB (89.77%) - LeetHub

--- LeetCode/Medium/0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ LeetCode/Medium/0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,16 +1,18 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
+        # DP list 정의 
+        # DP[i] = nums[:i+1]에서 nums[i]의 IS 순서
         DP = [1]*len(nums)
 
+        # DP list 채우기(DP[0]은 1이니 생략)
         for i in range(1,len(nums)):
-            
+
+            # 0부터 i까지 확인 
+            # nums[i]보다 작은 nums[j]의 경우 DP[j] + 1로 업데이트
             for j in range(i):
                 if nums[i] > nums[j]:
                     DP[i] = max(DP[i], DP[j] + 1)
         
-        
-        maxVal = 0
-        for val in DP:
-            maxVal = max(maxVal, val)
-
-        return maxVal
+        # DP list내에서 최대값 return
+        # 꼭 nums[i]에서 max가 나오는 것이 아님
+        return max(DP)
