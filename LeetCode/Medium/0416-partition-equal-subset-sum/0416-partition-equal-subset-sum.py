bb4a7525710f73964f90cc12cbc208bf9ba7c827
Coding_Pooh <tjdals4047@gmail.com>
2023-09-04 17:57:31 +0900

    Time: 1141 ms (44.13%), Space: 28.4 MB (44.17%) - LeetHub

--- LeetCode/Medium/0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ LeetCode/Medium/0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,17 +1,43 @@
 class Solution:
 
     def canPartition(self, nums: List[int]) -> bool:
-        total_sum = sum(nums)
         
-        if total_sum % 2 != 0:
-            return False
+        # 만약 전체 sum이 홀수면 동일하게 나눠질 수 없음
+        if sum(nums) % 2 != 0 : return False
+
+        # 주어진 list에서 중복없이 조합해서 전체 sum의 절반을 만들 수 있니?
+        targetSum = sum(nums) // 2
+
+        # DP[i] = targetSum이 i+1일 때, i+1을 만들 수 있는지 여부
+        # DP2[i] = i+1을 만들 수 있을 때, 어떤 num으로 만들었는지 담는 list 
+        DP = [False] * targetSum
+        DP2 = [[]] * targetSum
         
-        target_sum = total_sum // 2
-        dp = [False] * (target_sum + 1)
-        dp[0] = True
-        
-        for num in nums:
-            for i in range(target_sum, num - 1, -1):
-                dp[i] |= dp[i - num]
-        
-        return dp[target_sum]
+        # DP list 채우기
+        for i in range(targetSum):
+
+            # 만약 target과 동일한 값이 nums에 있으면 무조건 DP[i] = True이고 DP2[i]에 해당 값 추가
+            if (i+1) in nums:
+                left = nums[:]
+                left.remove(i+1)
+                DP[i], DP2[i] = True, left
+                continue
+            
+            # 만약 동일한 값이 아니라면 nums를 하나씩 돌면서 체크
+            for num in nums:
+
+                # i+1 - num에 해당하는 값을 만들어낼 수 있으면 가능성 있음
+                if (i+1)-num >= 1 and DP[i-num] == True:
+                    if num in DP2[i-num]:
+                        left = DP2[i-num][:]
+                        left.remove(num)
+                        DP[i], DP2[i] = True, left
+                        break
+
+        return DP[-1]
+
+
+
+
+ 
+
