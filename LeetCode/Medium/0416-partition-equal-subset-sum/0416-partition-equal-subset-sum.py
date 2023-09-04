aa30760245ce744e3bbe7064854c8b54f02c2c6d
Coding_Pooh <tjdals4047@gmail.com>
2023-09-04 16:17:03 +0900

    Time: 1083 ms (46.54%), Space: 16.3 MB (94.93%) - LeetHub

--- LeetCode/Medium/0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ LeetCode/Medium/0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,38 +1,17 @@
 class Solution:
 
     def canPartition(self, nums: List[int]) -> bool:
-
-        totalSum = sum(nums)
-
-        if totalSum % 2 != 0 : return False
-
-        targetSum = totalSum // 2
-
-        DP = [False] * targetSum
-        DP2 = [[]] * targetSum
+        total_sum = sum(nums)
         
-        for i in range(targetSum):
-            if (i+1) in nums:
-                DP[i], DP2[i] = True, [i+1]
-                continue
-            
-            for num in nums:
-                if (i+1)-num >= 1 and DP[i-num] == True:
-                    temp = nums[:]
-                    for selected in DP2[i-num]:
-                        temp.remove(selected)
-                    
-                    if num in temp:
-                        DP[i] = True
-                        # DP2[i] = DP2[i-num][:] 
-                        # DP2[i].append(num)
-                        DP2[i] = DP2[i-num] + [num]
-                        break
-
-        return DP[-1]
-
-
-
-
- 
-
+        if total_sum % 2 != 0:
+            return False
+        
+        target_sum = total_sum // 2
+        dp = [False] * (target_sum + 1)
+        dp[0] = True
+        
+        for num in nums:
+            for i in range(target_sum, num - 1, -1):
+                dp[i] |= dp[i - num]
+        
+        return dp[target_sum]
