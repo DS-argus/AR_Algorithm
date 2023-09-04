c378fb624c7b06dc9cec08dff99065ec4ebac070
Coding_Pooh <tjdals4047@gmail.com>
2023-09-04 22:22:37 +0900

    Time: 38 ms (79.35%), Space: 16.3 MB (40.47%) - LeetHub

--- LeetCode/Medium/0213-house-robber-ii/0213-house-robber-ii.py
+++ LeetCode/Medium/0213-house-robber-ii/0213-house-robber-ii.py
@@ -3,23 +3,16 @@ class Solution:
     # House Robber 1의 답
     # nums의 양끝이 연결되어 있지 않음
     def noRotationRob(self, nums: List[int]) -> int:
-        # DP list 정의
-        # DP[i] = nums[:i+1]에서 얻을 수 있는 maximum amount
+        
+        # DP list 정의 : DP[i] = nums[:i+1]에서 얻을 수 있는 maximum amount
         DP = [0] * len(nums)
 
-        # DP list 채우기
-        for i in range(len(nums)):
-            
-            # i == 0 , i == 1인 경우는 따로. 점화식에 DP[i-2] 들어가서
-            # DP[0] = nums[0]
-            if i == 0:
-                DP[i] = nums[i]
-                continue
+        # i == 0 , i == 1인 경우는 따로. 점화식에 DP[i-2] 들어가서
+        DP[0] = nums[0]
+        DP[1] = max(nums[0], nums[1])
 
-            # DP[1] = nums[0], nums[1] 중 더 큰 값
-            if i == 1:
-                DP[i] = max(nums[0], nums[1])
-                continue
+        # DP list 채우기
+        for i in range(2, len(nums)):
             
             # 1. nums[i-1]을 선택한 경우 : DP[i-1]과 동일
             # 2. nums[i-1]을 선택하지 않은 경우 : DP[i-2] + nums[i] 
@@ -38,13 +31,10 @@ class Solution:
         elif len(nums) == 2:
             return max(nums[0], nums[1])
 
-        elif len(nums) == 3:
-            return max(nums[0], nums[1], nums[2])
-
         else:
             # 1. 첫번째를 선택하지 않는 경우 : nums[1:]에서 max 찾기
             # 2. 첫번째를 선택하는 경우 : nums[2:-1]에서 max 찾기 + nums[0]
             return max(self.noRotationRob(nums[1:]), \
-                       self.noRotationRob(nums[2:-1]) + nums[0])
+                       self.noRotationRob(nums[:-1]))
 
     
