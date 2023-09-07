a7a8b43f8c5ed9e7720929ec521e486c5db9f814
Coding_Pooh <tjdals4047@gmail.com>
2023-09-07 15:50:52 +0900

    Time: 3 ms (44.14%), Space: 7.1 MB (19.72%) - LeetHub

--- LeetCode/Medium/0048-rotate-image/0048-rotate-image.cpp
+++ LeetCode/Medium/0048-rotate-image/0048-rotate-image.cpp
@@ -2,17 +2,11 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
 
-        for (int i = 0; i<matrix.size()/2; i++){
-            swap(matrix[i], matrix[matrix.size()-1-i]);
-        }
+        for (int i = 0; i<matrix.size()/2; i++)
+        swap(matrix[i], matrix[matrix.size()-1-i]);
 
-        transpose(matrix);
-
-    }
-private:
-    void transpose(vector<vector<int>>& matrix){
         for (int i = 0; i<matrix.size(); i++)
-            for (int j = i; j<matrix.size(); j++)
-                swap(matrix[i][j], matrix[j][i]);
+        for (int j = i; j<matrix.size(); j++)
+        swap(matrix[i][j], matrix[j][i]);
     }
 };
