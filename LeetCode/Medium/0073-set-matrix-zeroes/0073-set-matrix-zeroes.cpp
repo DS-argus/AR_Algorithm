34f07c239cd9bdf6bdcea6f1727c00c6fd7d246b
Coding_Pooh <tjdals4047@gmail.com>
2023-09-07 15:54:13 +0900

    Time: 12 ms (67.53%), Space: 13.4 MB (36.46%) - LeetHub

--- LeetCode/Medium/0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ LeetCode/Medium/0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -6,20 +6,19 @@ public:
         int* colZero = new int[matrix[0].size()]();
         
         for (int i = 0; i < matrix.size(); i++){
-            for (int j = 0; j < matrix[0].size(); j++){
-                if (rowZero[i] == 1 && colZero[j] == 1) continue;
-                if (matrix[i][j] == 0) {
-                    rowZero[i] = 1;
-                    colZero[j] = 1;
-                }
+        for (int j = 0; j < matrix[0].size(); j++){
+            if (rowZero[i] == 1 && colZero[j] == 1) continue;
+            if (matrix[i][j] == 0){
+                rowZero[i] = 1;
+                colZero[j] = 1;
             }
         }
 
-        for (int i = 0; i < matrix.size(); i++){
-            for (int j = 0; j < matrix[0].size(); j++){
-                if (rowZero[i] == 1 || colZero[j] == 1) matrix[i][j] = 0;
-            }
         }
+
+        for (int i = 0; i < matrix.size(); i++)
+        for (int j = 0; j < matrix[0].size(); j++)
+        if (rowZero[i] == 1 || colZero[j] == 1) matrix[i][j] = 0;
 
         delete[] rowZero;
         delete[] colZero;
