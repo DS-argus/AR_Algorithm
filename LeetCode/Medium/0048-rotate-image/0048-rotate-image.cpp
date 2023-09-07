51fcc796525350513eed84c6a5c53261b5588899
Coding_Pooh <tjdals4047@gmail.com>
2023-09-07 12:11:08 +0900

    Time: 4 ms (37.57%), Space: 7.1 MB (56.44%) - LeetHub

--- LeetCode/Medium/0048-rotate-image/0048-rotate-image.cpp
+++ LeetCode/Medium/0048-rotate-image/0048-rotate-image.cpp
@@ -11,10 +11,8 @@ public:
     }
 private:
     void transpose(vector<vector<int>>& matrix){
-        for (int i = 0; i<matrix.size(); i++){
-            for (int j = i; j<matrix.size(); j++){
+        for (int i = 0; i<matrix.size(); i++)
+            for (int j = i; j<matrix.size(); j++)
                 swap(matrix[i][j], matrix[j][i]);
-            }
-        }
     }
 };
