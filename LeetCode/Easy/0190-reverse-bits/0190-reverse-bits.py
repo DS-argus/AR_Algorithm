704a463d1c97e70a7ef0482bb8b434999933c634
Coding_Pooh <tjdals4047@gmail.com>
2023-09-12 18:56:13 +0900

    Time: 34 ms (91.31%), Space: 16.3 MB (57.89%) - LeetHub

--- LeetCode/Easy/0190-reverse-bits/0190-reverse-bits.py
+++ LeetCode/Easy/0190-reverse-bits/0190-reverse-bits.py
@@ -1,6 +1,3 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
-        reversedBits = 0
-        for i in range(32):
-            reversedBits += (1<<(31-i))*((n>>i)%2)
-        return reversedBits
+        return int(bin(n)[2:].zfill(32)[::-1], 2)
