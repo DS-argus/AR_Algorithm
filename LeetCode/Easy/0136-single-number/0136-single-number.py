dbaedd71950f54e102d62f6378bfaacac3aba07b
Coding_Pooh <tjdals4047@gmail.com>
2023-09-12 17:47:38 +0900

    Time: 501 ms (19.09%), Space: 19.2 MB (10.73%) - LeetHub

--- LeetCode/Easy/0136-single-number/0136-single-number.py
+++ LeetCode/Easy/0136-single-number/0136-single-number.py
@@ -1,9 +1,11 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
+        minVal = 3*10**4
+        for num in nums:
+            minVal = min(minVal, num)
         
         bits = 0
-        
         for num in nums:
-            bits ^= 2 ** (num-min(nums))
+            bits ^= 2 ** (num-minVal)
 
-        return len(bin(bits))-3+min(nums)
+        return len(bin(bits))-3+minVal
