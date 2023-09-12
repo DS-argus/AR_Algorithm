9de7bd4c06ccc2ffda67d4ed167ea4aa1cf312e2
Coding_Pooh <tjdals4047@gmail.com>
2023-09-12 19:14:44 +0900

    Time: 219 ms (16.3%), Space: 17.7 MB (48.3%) - LeetHub

--- LeetCode/Easy/0268-missing-number/0268-missing-number.py
+++ LeetCode/Easy/0268-missing-number/0268-missing-number.py
@@ -1,6 +1,16 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        missing = len(nums)
-        for i, num in enumerate(nums):
-            missing ^= i ^ num
-        return missing
+        bits = (1<<(len(nums)+1))-1
+
+        for num in nums:
+            bits -= (1<<num)
+        
+        i= 0
+        while bits % 2 != 1:
+            bits >>= 1
+            i+=1
+        
+        return i
+            
+            
+        
