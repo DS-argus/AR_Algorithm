c0a0cc45104f0dfc288f706d20cd1106ac404f54
Coding_Pooh <tjdals4047@gmail.com>
2023-09-12 16:19:29 +0900

    Time: 4839 ms (5.01%), Space: 18.8 MB (91.03%) - LeetHub

--- LeetCode/Easy/0136-single-number/0136-single-number.py
+++ LeetCode/Easy/0136-single-number/0136-single-number.py
@@ -1,7 +1,7 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         bits = 0
-        for i in range(len(nums)):
-            bits ^= 2 ** (nums[i]+3*10**4)
+        for num in nums:
+            bits ^= 2 ** (num+3*10**4)
         
         return len(bin(bits))-30003
