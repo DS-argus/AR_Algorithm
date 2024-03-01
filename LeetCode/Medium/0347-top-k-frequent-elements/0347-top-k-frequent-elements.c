851ba2baf5df5212acefcb7110a940ca04b5e39c
Coding_Pooh <tjdals4047@gmail.com>
2024-03-02 00:34:54 +0900

    Time: 10 ms (86.14%), Space: 8.1 MB (90.10%) - LeetHub

---
+++ LeetCode/Medium/0347-top-k-frequent-elements/0347-top-k-frequent-elements.c
@@ -0,0 +1,56 @@
+//int array에서 특정 int를 제거해주는, void 함수
+void removeElement_int(int* nums, int *numsSize, int val){
+    int index = 0;
+    for (int i = 0; i < *numsSize; i++) {
+        if (nums[i] != val) {
+            nums[index++] = nums[i];
+        }
+    }
+    *numsSize = index;
+}
+
+
+int* topKFrequent(int* nums, int numsSize, int k, int* returnSize) {
+    int MIN = 1e4;
+    int MAX = -1e4;
+
+    // find min, max from given array
+    for(int i = 0; i < numsSize; i ++){
+        if (nums[i] < MIN) MIN = nums[i];
+        if (nums[i] > MAX) MAX = nums[i];
+    }
+
+    // MAX - MIN + 1만큼의 array 할당
+    int range = MAX-MIN+1;
+    int* counter = (int*)malloc(sizeof(int) * range);
+    memset(counter, 0, sizeof(int) * range); // malloc 사용 시 초기화
+    
+    // index에 해당하는 자리에 개수 counting
+    for(int i = 0; i < numsSize; i ++){
+        counter[nums[i]-MIN]++;
+    }
+
+    int* result = (int*)malloc(sizeof(int) * k);
+    *returnSize = k;
+
+    int i = 0;
+    int returnsize = 0;
+    while (i<k){
+        int MAX = -1;
+        int idx = -1;
+        
+        for(int j = 0; j < range; j++){
+            if(counter[j] > MAX) {
+                MAX = counter[j];
+                idx = j;
+            }
+        }
+
+        result[i] = idx+MIN;
+        counter[idx] = 0;
+        i++;
+    }
+
+    return result;
+}
+
