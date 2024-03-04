a7a3b90ae8a86abd45c98dcc1a7cf52e26ce3af8
Coding_Pooh <tjdals4047@gmail.com>
2024-03-04 15:55:34 +0900

    Time: 48 ms (12.86%), Space: 16.9 MB (29.94%) - LeetHub

--- LeetCode/Medium/0046-permutations/0046-permutations.py
+++ LeetCode/Medium/0046-permutations/0046-permutations.py
@@ -1,23 +1,44 @@
-class Solution(object):
-    
-    def permuteHelper(self, nums, subnums, results):
-        if len(subnums) == len(nums):
-            results.append(subnums)
-            return
+class Solution:
+
+    def permute(self, nums: List[int]) -> List[List[int]]:
+
+        results = []
+        subset = []
+        q = [subset]
+
+        while q:
+
+            subset = q.pop(0)
+
+            if len(subset) == len(nums):
+                results.append(subset.copy())
+            
+            for num in nums:
+                if num not in subset:
+                    q.append(subset.copy()+[num])
+
+        return results
+
+
+
+
+#      def permuteHelper(self, nums, subnums, results):
+#         if len(subnums) == len(nums):
+#             results.append(subnums)
+#             return
         
-        for num in nums:
-            if num not in subnums:
-                self.permuteHelper(nums, subnums+[num], results)
+#         for num in nums:
+#             if num not in subnums:
+#                 self.permuteHelper(nums, subnums+[num], results)
                 
     
-    def permute(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
-        results = []
+#     def permute(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: List[List[int]]
+#         """
+#         results = []
         
-        self.permuteHelper(nums, [], results)
+#         self.permuteHelper(nums, [], results)
         
-        return results
-        
+#         return results
