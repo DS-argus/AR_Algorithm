9aa3f24098c4624e9dbef28213318a2709251db0
Coding_Pooh <tjdals4047@gmail.com>
2024-03-05 11:48:12 +0900

    Time: 70 ms (57.35%), Space: 17.2 MB (98.58%) - LeetHub

--- LeetCode/Medium/1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.py
+++ LeetCode/Medium/1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.py
@@ -11,12 +11,37 @@ class Solution:
             while s[lr] == char and lr+1 < len(s):
                 lr += 1
             
-            while s[rl-1] == char and rl > 0:
+            while s[rl-1] == char and rl > lr:
                 rl -= 1
             
-            s = s[lr:rl]
-            
-            ll, rr = 0, len(s)
+            ll, rr = lr, rl
 
-        return len(s)
-        
+        return rr - ll
+
+    
+# int minimumLength(char* s) {
+#     int ll = 0;
+#     int rr = strlen(s); // Get the length of the string.
+
+#     while (ll+1 < rr && s[ll] == s[rr - 1]) {
+#         char ch = s[ll];
+#         int lr = ll, rl = rr;
+
+#         // Increment lr until a different character or the end of the substring is found.
+#         while (lr+1 < rl && s[lr] == ch) {
+#             lr++;
+#         }
+
+#         // Decrement rl until a different character or the start of the substring is found.
+#         while (rl > lr && s[rl - 1] == ch) {
+#             rl--;
+#         }
+
+#         // Update ll and rr for the next iteration or result calculation.
+#         ll = lr;
+#         rr = rl;
+#     }
+
+#     // The length of the remaining string is rr - ll.
+#     return rr - ll;
+# }
