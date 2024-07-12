6758c792787952f99e5f908cf4e492f4190b16d1
Coding_Pooh <tjdals4047@gmail.com>
2024-07-12 19:36:38 +0900

    [Gold V] Title: 로봇 청소기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
@@ -5,34 +5,28 @@ space = [list(map(int, input().split())) for _ in range(N)]
 move = [(-1, 0), (0, 1), (1, 0), (0, -1)]  # 0, 1, 2, 3 : 북, 동, 남, 서
 cnt = 0
 stack = [(r, c, d)]
+
 while stack:
-    flag = False
     r, c, d = stack.pop()
     if not space[r][c]:
         space[r][c] = 2
         cnt += 1
 
-    valid_moves = []
+    is_valid = False
     for i in range(4):
         dr, dc = move[d-i-1]
         nr, nc = r + dr, c + dc
 
-        if 0 <= nr < N and 0 <= nc < M and (not space[nr][nc]): valid_moves.append((nr, nc, (d-i-1)%4))
+        if 0 <= nr < N and 0 <= nc < M and (not space[nr][nc]):
+            if not is_valid: stack.append((nr, nc, (d-i-1)%4))
+            is_valid = True
 
-    if not valid_moves:
-        # breakpoint()
+    if not is_valid:
         dr, dc = move[(d-2)%4]
         nr, nc = r+dr, c+dc
-        if nr < 0 or nr >= N or nc < 0 or nc >= M or space[nr][nc] == 1:  
+        if nr < 0 or nr >= N or nc < 0 or nc >= M or space[nr][nc] == 1: 
             break
         else:
             stack.append((nr, nc, d))
-
-    else:
-        stack.append(valid_moves[0])
-
-
-# for i in range(N):
-#     print(space[i])
 
 print(cnt)
