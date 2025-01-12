54286bb160da0b8e82bad8c1df14739a9eb831e8
Soungmin Park <tjdals4047@gmail.com>
2025-01-12 13:50:51 +0900

    [Silver II] Title: 스택 수열, Time: 232 ms, Memory: 43440 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.py
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -6,22 +6,22 @@ INF = sys.maxsize
 from collections import deque
 
 N = int(input())
-orders = [int(input()) for _ in range(N)]
+orders = deque([int(input()) for _ in range(N)])
 
-inputs = list(range(1, N + 1))
-stack = deque()
+inputs = deque(range(1, N + 1))
+stack = []
 result = []
 flag = False
 
 while stack or inputs:
     if not stack:
         stack.append(inputs[0])
-        inputs.pop(0)
+        inputs.popleft()
         result.append("+")
 
     elif not inputs:
         # 실패
-        if list(stack) != orders[::-1]:
+        if list(stack) != list(orders)[::-1]:
             flag = True
             break
         # 성공 : pop만 해주면 됨
@@ -34,12 +34,12 @@ while stack or inputs:
         num = inputs[0]
         if stack[-1] < orders[0]:
             stack.append(num)
-            inputs.pop(0)
+            inputs.popleft()
             result.append("+")
 
         elif stack[-1] == orders[0]:
             stack.pop()
-            orders.pop(0)
+            orders.popleft()
             result.append("-")
 
         else:
