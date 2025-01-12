2775beb2d6a40cad7fea4756415aae5be24289c9
Soungmin Park <tjdals4047@gmail.com>
2025-01-12 14:01:23 +0900

    [Silver II] Title: 스택 수열, Time: 132 ms, Memory: 41736 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.py
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -14,42 +14,16 @@ result = []
 flag = False
 
 while stack or inputs:
-    if not stack:
-        stack.append(inputs[0])
-        inputs.popleft()
+    if stack and stack[-1] == orders[0]:
+        stack.pop()
+        orders.popleft()
+        result.append("-")
+
+    elif inputs:
+        stack.append(inputs.popleft())
         result.append("+")
-
-    elif not inputs:
-        # 실패
-        if list(stack) != list(orders)[::-1]:
-            flag = True
-            break
-        # 성공 : pop만 해주면 됨
-        else:
-            while stack:
-                result.append("-")
-                stack.pop()
-
     else:
-        num = inputs[0]
-        if stack[-1] < orders[0]:
-            stack.append(num)
-            inputs.popleft()
-            result.append("+")
+        print("NO")
+        exit()
 
-        elif stack[-1] == orders[0]:
-            stack.pop()
-            orders.popleft()
-            result.append("-")
-
-        else:
-            flag = True
-            break
-
-
-if flag:
-    print("NO")
-
-else:
-    for v in result:
-        print(v)
+print("\n".join(result))
