507bc9815e27b36784b5fadaa80d7e7a6a1e5f7e
Soungmin Park <tjdals4047@gmail.com>
2025-02-07 22:08:25 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 4, Time: 1148 ms, Memory: 86084 KB -BaekjoonHub

---
+++ 백준/Silver/24482. 알고리즘 수업 － 깊이 우선 탐색 4/알고리즘 수업 － 깊이 우선 탐색 4.py
@@ -0,0 +1,34 @@
+import sys
+
+sys.setrecursionlimit(1000000)
+input = sys.stdin.readline
+
+from collections import defaultdict, deque
+
+N, M, R = map(int, input().split())
+
+EDGES = [tuple(map(int, input().split())) for _ in range(M)]
+EDGES.sort(key=lambda x: (x[0], x[1]), reverse=True)
+
+adj = defaultdict(list)
+
+for a, b in EDGES:
+    adj[a].append(b)
+    adj[b].append(a)
+
+depth = [-1] * N
+
+
+def dfs(start: int, d: int):
+
+    depth[start - 1] = d
+
+    for neighbor in adj[start]:
+        if depth[neighbor - 1] == -1:
+            dfs(neighbor, d + 1)
+
+
+dfs(R, 0)
+
+for v in depth:
+    print(v)
