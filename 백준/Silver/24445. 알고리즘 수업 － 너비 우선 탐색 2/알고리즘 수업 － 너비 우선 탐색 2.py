e940cd87d91119743e63e136fbcae10d008a32c6
Soungmin Park <tjdals4047@gmail.com>
2025-02-07 21:56:10 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 916 ms, Memory: 82500 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.py
@@ -0,0 +1,32 @@
+import sys
+
+input = sys.stdin.readline
+
+from collections import defaultdict, deque
+
+N, M, R = map(int, input().split())
+
+EDGES = [tuple(map(int, input().split())) for _ in range(M)]
+EDGES.sort(key=lambda x: (x[0], x[1]), reverse=True)
+
+adj = defaultdict(list)
+
+for a, b in EDGES:
+    adj[a].append(b)
+    adj[b].append(a)
+
+visit_order = [0] * N
+visit_order[R - 1] = 1
+q = deque([R])
+order = 1
+while q:
+    cnt = q.popleft()
+
+    for neighbor in adj[cnt]:
+        if visit_order[neighbor - 1] == 0:
+            q.append(neighbor)
+            visit_order[neighbor - 1] = order + 1
+            order += 1
+
+for v in visit_order:
+    print(v)
