0c22fbbfe44652f46800ae9478965111ab9e03be
Soungmin Park <tjdals4047@gmail.com>
2025-03-03 21:21:20 +0900

    [Silver III] Title: N과 M (7), Time: 1304 ms, Memory: 34908 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.py
@@ -0,0 +1,23 @@
+import sys
+from collections import defaultdict
+
+sys.setrecursionlimit(1000000)
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+
+numbers = list(map(int, input().split()))
+numbers.sort()
+
+
+def recursive(ans: list, idx: int):
+    if len(ans) == M:
+        print(" ".join(map(str, ans)))
+        return
+
+    for i in range(N):
+        recursive(ans + [numbers[i]], i)
+
+
+for idx in range(N):
+    recursive([numbers[idx]], idx)
