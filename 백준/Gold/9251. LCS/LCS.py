c7f2042d2317c42d6be0eb19257af955418dd079
Soungmin Park <tjdals4047@gmail.com>
2025-03-04 00:22:53 +0900

    [Gold V] Title: LCS, Time: 468 ms, Memory: 58460 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -7,30 +7,16 @@ input = sys.stdin.readline
 s1 = input().rstrip()
 s2 = input().rstrip()
 
+l1, l2 = len(s1), len(s2)
 
-dp = [[-1 for _ in range(len(s2))] for _ in range(len(s1))]
+dp = [[0 for _ in range(l2 + 1)] for _ in range(l1 + 1)]
 
-dp[-1][-1] = int(s1[-1] == s2[-1])
-
-for i in range(len(s1) - 1, -1, -1):
-    for j in range(len(s2) - 1, -1, -1):
-        if i == len(s1) - 1 and j == len(s2) - 1:
-            continue
-
+for i in range(l1 - 1, -1, -1):
+    for j in range(l2 - 1, -1, -1):
         if s1[i] == s2[j]:
-            if i == len(s1) - 1 or j == len(s2) - 1:
-                dp[i][j] = 1
-
-            else:
-                dp[i][j] = dp[i + 1][j + 1] + 1
+            dp[i][j] = dp[i + 1][j + 1] + 1
 
         else:
-            if i == len(s1) - 1:
-                dp[i][j] = dp[i][j + 1]
-            elif j == len(s2) - 1:
-                dp[i][j] = dp[i + 1][j]
-
-            else:
-                dp[i][j] = max(dp[i + 1][j], dp[i][j + 1])
+            dp[i][j] = max(dp[i + 1][j], dp[i][j + 1])
 
 print(dp[0][0])
