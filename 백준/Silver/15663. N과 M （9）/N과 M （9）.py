4dfea6e50c13f5e074e111486afb6485e89e710d
Soungmin Park <tjdals4047@gmail.com>
2025-03-03 22:27:05 +0900

    [Silver II] Title: N과 M (9), Time: 68 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.py
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -1,39 +1,23 @@
 import sys
-from collections import defaultdict
 
-sys.setrecursionlimit(1000000)
+sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
+numbers = sorted(map(int, input().split()))  # 입력을 정렬하여 중복 방지
 
-numbers = list(map(int, input().split()))
-numbers.sort()
-
-
-def recursive(ans: list, left: list):
+def backtrack(ans, used):
     if len(ans) == M:
-        results.append(ans)
+        print(" ".join(map(str, ans)))
         return
 
     prev = -1
-    for l in left:
-        if l != prev:
-            new_left = left[:]
-            new_left.remove(l)
+    for i in range(N):
+        if not used[i] and numbers[i] != prev:
+            used[i] = True
+            backtrack(ans + [numbers[i]], used)
+            used[i] = False
+            prev = numbers[i]
 
-            recursive(ans + [l], new_left)
-            prev = l
-
-
-results = []
-
-prev = -1
-for idx in range(N):
-    if prev != numbers[idx]:
-        left = numbers[:]
-        left.remove(numbers[idx])
-        recursive([numbers[idx]], left)
-        prev = numbers[idx]
-
-for result in results:
-    print(" ".join(map(str, result)))
+used = [False] * N
+backtrack([], used)
