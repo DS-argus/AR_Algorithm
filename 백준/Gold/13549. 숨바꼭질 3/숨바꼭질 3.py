78d58a1a435dcca7b41fecfcb9b8198fa8aa9e1b
Soungmin Park <tjdals4047@gmail.com>
2025-03-06 23:30:59 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 148 ms, Memory: 38340 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -1,48 +1,37 @@
 import heapq as hq
 import sys
-from collections import defaultdict
 
 sys.setrecursionlimit(1000000)
 input = sys.stdin.readline
 
 N, K = map(int, input().split())
 
-V = 100_001
-
+V = 100_001  # 최대 범위
 COST = [sys.maxsize] * V
-visit = [False] * V
 
 heap = [(0, N)]
+COST[N] = 0  # 시작점 비용 0
 
 while heap:
     time, node = hq.heappop(heap)
-
-    if visit[node - 1]:
-        continue
 
     if node == K:
         print(time)
         exit()
 
-    visit[node - 1] = True
+    # ✅ 순간이동 (가중치 0)
+    if 2 * node <= 100_000 and COST[2 * node] > time:
+        COST[2 * node] = time
+        hq.heappush(heap, (time, 2 * node))
 
-    if 0 <= node + 1 <= 100_000:
-        # X+1
-        if not visit[node] and COST[node] > time + 1:
-            COST[node] = time + 1
-            hq.heappush(heap, (COST[node], node + 1))
+    # ✅ X+1 (가중치 1)
+    if node < 100_000 and COST[node + 1] > time + 1:
+        COST[node + 1] = time + 1
+        hq.heappush(heap, (time + 1, node + 1))
 
-    if 0 <= node - 1 <= 100_000:
-        # X-1
-        if not visit[node - 2] and COST[node - 2] > time + 1:
-            COST[node - 2] = time + 1
-            hq.heappush(heap, (COST[node - 2], node - 1))
-
-    if 0 <= 2 * node <= 100_000:
-        # 2X
-        if not visit[2 * node - 1] and COST[2 * node - 1] > time:
-            COST[2 * node - 1] = time
-            hq.heappush(heap, (COST[2 * node - 1], 2 * node))
-
+    # ✅ X-1 (가중치 1)
+    if node > 0 and COST[node - 1] > time + 1:
+        COST[node - 1] = time + 1
+        hq.heappush(heap, (time + 1, node - 1))
 
 print(-1)
