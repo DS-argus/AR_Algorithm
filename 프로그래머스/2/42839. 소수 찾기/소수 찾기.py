8382ccdc81072f61f76c027a68de97f46337df5d
Soungmin Park <tjdals4047@gmail.com>
2025-03-21 09:29:56 +0900

    [level 2] Title: 소수 찾기, Time: 2266.21 ms, Memory: 9.47 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.py
@@ -2,16 +2,15 @@ def solution(numbers):
     
     answers = []
     
-    def isPrime(n: int):
-        if n == 1 or n == 0:
+    def isPrime(n: int) -> bool:
+        if n < 2:  
             return False
-        prime = True
-        for i in range(2, int(n**(0.5))+1):
+
+        for i in range(2, n):
             if n % i == 0:
-                prime = False
-                break
-        
-        return prime
+                return False 
+
+        return True
                        
     
     def recursive(string: str, left: str):
